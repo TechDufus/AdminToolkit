a21d7f1faa0dc3b934665f9a517bcb963eaba4f6
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 14:19:51 -0500

    Added help.

--- Functions/Public/Get-PrintManagement.ps1
+++ Functions/Public/Get-PrintManagement.ps1
@@ -3,10 +3,14 @@
 <#
 .SYNOPSIS
     Quickly launch Print Management MSC Snap-in
-
 .DESCRIPTION
     Opens Print Management for the local PC and one remote PC using -ComputerName
+.EXAMPLE
+    PS> Get-PrintManagement -ComputerName Computer1
 
+    Description
+    -----------
+    This will open PrintManagement on the local machine and connect to the remote `Computer1`
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
@@ -18,6 +22,5 @@ Function Get-PrintManagement {
     )
     printmanagement.msc /computer:$ComputerName
 }
-
 
 
