c8f7ccfddb3267cf7be5e2e91a296bad02b71bab
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 10:03:37 -0500

    Updated tests to work in Pester 5.0

--- Tests/AdminToolkit_Module.Tests.ps1
+++ Tests/AdminToolkit_Module.Tests.ps1
@@ -1,73 +1,91 @@
+#Requires -Modules @{ModuleName="Pester";ModuleVersion="5.0.0"}
 
-$ModuleName = "AdminToolkit"
-$ModuleManifestName = "$ModuleName.psd1"
-
-$ModuleManifestPath = [System.IO.Path]::Combine($PSScriptRoot,"..","$ModuleManifestName")
-$ModulePath = [System.IO.Path]::Combine($PSScriptRoot,"..")
-$AliasesPath = [System.IO.Path]::Combine($PSScriptRoot,"..","Functions","Public","Aliases.ps1")
-
-$PublicFunctionsFiles = [System.IO.Path]::Combine($PSScriptRoot,"..","Functions","Public","*.ps1")
-$PublicFileNames = Get-ChildItem $PublicFunctionsFiles -Exclude *tests.ps1, Aliases.ps1 | ForEach-Object {
-    $_.BaseName
-}
-$PrivateFunctionsFiles = [System.IO.Path]::Combine($PSScriptRoot,"..","Functions","Private","*.ps1")
-$PrivateFileNames = Get-ChildItem $PrivateFunctionsFiles -Exclude *tests.ps1 | ForEach-Object {
-    $_.BaseName
-}
-
-if (Get-Module $ModuleName) {
-    Remove-Module $ModuleName
-}
-
-Import-Module $ModuleManifestPath
-
-Describe "$ModuleName Module Tests" {
+Import-Module ([System.IO.Path]::Combine($PSScriptRoot,'..','AdminToolkit.psd1')) -Force
+Describe "AdminToolkit Module Public Tests" {
     It "Imports Successfully" {
-        Import-Module $ModuleManifestPath -Force
-        Get-Module $ModuleName | Should -Be $true
+        Get-Module AdminToolkit | Should -Not -BeNullOrEmpty
     }
     Context 'Public Functions' {
         It 'should import successfully' {
-            $PublicImportedCommands = (Get-Command -Module $ModuleName).Name
-            $PublicImportFailedFunctions = (Compare-Object $PublicImportedCommands $PublicFileNames).InputObject
-            $PublicImportFailedFunctions | Should -Be $null
+            $PublicImportedCommands = (Get-Command -Module AdminToolkit).Name
+            $PublicFiles = Get-ChildItem ([System.IO.Path]::Combine($PSScriptRoot,'..','Functions','Public','*.ps1')) -Exclude *tests.ps1, Aliases.ps1 | ForEach-Object {
+                                $_
+                            }
+            $PublicImportFailedFunctions = (Compare-Object $PublicImportedCommands $($PublicFiles).BaseName).InputObject
+            $PublicImportFailedFunctions | Should -BeNullOrEmpty
         }
-    }
-    Context 'Private Functions' {
-        It 'should import successfully - Haven''t configured test yet' {
-            #$PrivateImportedCommands = (Get-Command -Module $ModuleName).Name
-            #$PrivateImportFailedFunctions = (Compare-Object $PrivateImportedCommands $PrivateFileNames).InputObject
-            $true | Should -Be $true
+
+        $PublicFiles = Get-ChildItem ([System.IO.Path]::Combine($PSScriptRoot,'..','Functions','Public','*.ps1')) -Exclude *tests.ps1, Aliases.ps1 | ForEach-Object {
+            Context "Test File: $($_.BaseName)" {
+                $PSDefaultParameterValues = @{
+                    "It:TestCases" = @{ CurrentFunction = $_ }
+                }
+                It "Should have comment-based help block" {
+                    $CurrentFunction.FullName | Should -FileContentMatch '<#'
+                    $CurrentFunction.FullName | Should -FileContentMatch '#>'
+                }
+                It "Should have SYNOPSIS section in help" {
+                    $CurrentFunction.FullName | Should -FileContentMatch '.SYNOPSIS'
+                }
+                It "Should have DESCRIPTION section in help" {
+                    $CurrentFunction.FullName | Should -FileContentMatch '.DESCRIPTION'
+                }
+                It "Should have EXAMPLE section in help" {
+                    $CurrentFunction.FullName | Should -FileContentMatch '.EXAMPLE'
+                }
+                It "Should be an advanced function" {
+                    $CurrentFunction.FullName | Should -FileContentMatch 'function'
+                    $CurrentFunction.FullName | Should -FileContentMatch 'cmdletbinding'
+                    $CurrentFunction.FullName | Should -FileContentMatch 'param'
+                }
+                It "Should be valid PowerShell code" {
+                    $FileContent = Get-Content -Path $CurrentFunction.FullName -ErrorAction Stop
+                    $Errors = $null
+                    $null = [System.Management.Automation.PSParser]::Tokenize($FileContent, [ref]$errors)
+                    $errors.Count | Should -be 0
+                }
+            }
         }
     }
     Context 'Aliases' {
         It 'should import successfully' {
+            $AliasesPath = [System.IO.Path]::Combine($PSScriptRoot,'..','Functions','Public','Aliases.ps1')
             if (Test-Path $AliasesPath) {
                 $ModuleAliases = (Get-Content $AliasesPath | Select-String "Set-Alias").Count
-                $ActualAliases = (Get-Command -Module $ModuleName -CommandType Alias).Count
+                $ActualAliases = (Get-Command -Module AdminToolkit -CommandType Alias).Count
             } else {
                 $ModuleAliases = 0
                 $ActualAliases = 0
             }
-            $ActualAliases | Should -Be $ModuleAliases
+            $ActualAliases | Should -Match $ModuleAliases
         }
     }
     Context 'Files' {
         It 'LICENSE should exist' {
-            $LicenseFile = [System.IO.Path]::Combine($ModulePath,'LICENSE')
+            $LicenseFile = [System.IO.Path]::Combine($PSScriptRoot,'..','LICENSE')
             $isLicense = Get-ChildItem $LicenseFile
             $isLicense | Should -Be $true
         }
         It 'CHANGELOG.md should exist' {
-            $ChangelogFile = [System.IO.Path]::Combine($ModulePath,'CHANGELOG.md')
+            $ChangelogFile = [System.IO.Path]::Combine($PSScriptRoot,'..','CHANGELOG.md')
             $isChangelog = Get-ChildItem $ChangelogFile
             $isChangelog | Should -Be $true
         }
     }
 }
 
-
-
-if (Get-Module $ModuleName) {
-    Remove-Module $ModuleName
+InModuleScope AdminToolkit {
+    Describe "AdminToolkit Module Private Tests" {
+        Context 'Private Functions' {
+            It 'should import successfully' {
+                $PrivateImportedCommands = (Get-Command -Module AdminToolkit).Name
+                $PrivateFiles = Get-ChildItem ([System.IO.Path]::Combine($PSScriptRoot,'..','Functions','Private','*.ps1')) -Exclude *tests.ps1, Aliases.ps1 | ForEach-Object {
+                                    $_
+                                }
+                $PrivateImportSuccessfulFunctions = Compare-Object $PrivateImportedCommands $PrivateFiles.BaseName -IncludeEqual -ExcludeDifferent
+                $PrivateImportSuccessfulFunctions.InputObject | Should -Be $PrivateFiles.BaseName
+            }
+        }
+    }
 }
+
