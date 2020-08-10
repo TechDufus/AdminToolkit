85a384b5f32e7c2aaca7978442686014921df17f
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 14:06:45 -0500

    Updating comments and making an advanced function.

--- Functions/Public/Clear-Arp.ps1
+++ Functions/Public/Clear-Arp.ps1
@@ -2,16 +2,22 @@
 
 <#
 .SYNOPSIS
-Use Netsh to clear ArpCache
-
+    Use Netsh to clear ArpCache
 .DESCRIPTION
-Clears the local arp table
+    Clears the local arp table
+.EXAMPLE
+    PS> Clear-Arp
 
+    Description
+    -----------
+    This will clear the arpcache on the local machine.
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
 #>
 function Clear-Arp() {
+    [CmdletBinding()]
+    param()
     netsh.exe interface ip delete arpcache
 }
 
