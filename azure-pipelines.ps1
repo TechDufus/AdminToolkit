b4019badcf2159e6886b862789e472f4fe3738b6
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 12:39:05 -0500

    Creating pipeline script.

---
+++ azure-pipelines.ps1
@@ -0,0 +1,6 @@
+      $PesterModule = Get-Module -Name Pester -ListAvailable
+      if (-Not($PesterModule.Version -gt '5.0.0')) {
+        Install-Module -Name Pester -MinimumVersion 5.0.0 -Force -SkipPublisherCheck
+      }
+      
+      Invoke-Pester "$PSScriptRoot/Tests/" -OutputFile "./Test-Pester.XML" -OutputFormat "NUnitXML"
