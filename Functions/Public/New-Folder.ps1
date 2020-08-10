671ec8f496b2b69e96dd1682dabd5490cc217be5
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 14:49:29 -0500

    Added help and made an advanced function.

--- Functions/Public/New-Folder.ps1
+++ Functions/Public/New-Folder.ps1
@@ -3,11 +3,24 @@
 <#
 .SYNOPSIS
     Easily create a new folder in the current working directory.
+.DESCRIPTION
+    This will create a new directory in the current working directory.
+.EXAMPLE
+    PS> New-Folder Foobar
+    
+    Description
+    -----------
+    This will create a new folder 'Foobar' if there currently is not a folder of the same name.
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
 #>
-function New-Folder($FolderName) {
+function New-Folder() {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,Position=0)
+        [string] $FolderName
+    )
     if (-Not (Test-Path -Path ".\$FolderName")) {
         New-Item -Name $FolderName -ItemType Directory
     }
