d8e2a5bb4348cb4954b1bc06f45a3859110c42eb
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 14:18:10 -0500

    Adding help.

--- Functions/Public/grep.ps1
+++ Functions/Public/grep.ps1
@@ -1,11 +1,16 @@
+!#/usr/bin/env pwsh
+
 <#
 .SYNOPSIS
     Basic version of the linux command `grep` on Windows.
+.DESCRIPTION
+    This is a windows version of the linux `grep` command. I still need to figure out how to NOT import this command when on a linux system.
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
 #>
 function grep {
+    [CmdletBinding()]
     param(
         $regex
     )
@@ -14,6 +19,3 @@ function grep {
         $_ | Where-Object { $_ -match $regex }
     }
 }
-
-
-
