caf4265fc669594a26cd6c55a35289f8ca533603
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 15:08:08 -0500

    Added help and made an advanced function.

--- Functions/Public/Update-PowerShell.ps1
+++ Functions/Public/Update-PowerShell.ps1
@@ -9,11 +9,24 @@
     Specifying this switch will install the latest preview version of PowerShell. Otherwise this will install / update the latest stable release.
 .PARAMETER Quiet
     Specifying this switch will install or update quietly with no gui popup, taking all defaults of the install. You need to run as admin to use this switch.
+.EXAMPLE
+    PS> Update-Powershell -Preview
+    
+    Description
+    -----------
+    This will update or install PowerShell with the latest Preview release.
+.EXAMPLE
+    PS> Update-Powershell -Quiet
+    
+    Description
+    -----------
+    This will update or install the latest General Release version of PowerShell.
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
 #>
 function Update-Powershell() {
+    [CmdletBinding()
     param(
         [switch] $Preview,
         [switch] $Quiet
