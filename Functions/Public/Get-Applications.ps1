44daac8291707d3c923f5b043c40907b48762b78
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 14:09:09 -0500

    Updating help and making an advanced function.

--- Functions/Public/Get-Applications.ps1
+++ Functions/Public/Get-Applications.ps1
@@ -6,11 +6,19 @@
 
 .DESCRIPTION
     Query local registry for installed applications.
+.EXAMPLE
+    PS> Get-Applications
+
+    Description
+    -----------
+    This will generate all installed applications on the local system.
 .NOTES
     Author: Matthew J. DeGarmo
     Site: https://matthewjdegarmo.github.io
 #>
 Function Get-Applications {
+    [CmdletBinding()]
+    param()
     Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
     Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
     Sort-Object DisplayName
