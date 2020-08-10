13dc5c49c690a004ef94b0ecb9cd21c7c0c666d0
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 14:21:34 -0500

    Added help.

--- Functions/Public/grep.ps1
+++ Functions/Public/grep.ps1
@@ -5,6 +5,13 @@
     Basic version of the linux command `grep` on Windows.
 .DESCRIPTION
     This is a windows version of the linux `grep` command. I still need to figure out how to NOT import this command when on a linux system.
+    This is basically a shorter `Select-String`, and does not support other grep flags as on a Linux system.
+.EXAMPLE
+    Get-Process | grep powershell
+    
+    Description
+    -----------
+    This will filter the `Get-Process` output with the regex 'powershell'.
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
