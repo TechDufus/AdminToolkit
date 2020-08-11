22e510d5c4dba94b8eb76b4fb28b367ca88dede7
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-11 18:53:55 -0500

    Updating Module path information.

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -1,15 +1,10 @@
 $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
-$CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine($PSScriptRoot,'..','AdminToolkit.psd1'))
+$CurrentModule = Test-ModuleManifest -Path (Join-Path -Path "AdminToolkit" -ChildPath "AdminToolkit.psd1")
 
 if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
-  #PIPELINE MODULE HAS A NEW MANIFEST VERSION - PUBLISH TO PSGALLERY OR INITIAL PUBLISHING TO PSGALLERY
-  #$PWDParentDir = Split-Path $PSScriptRoot -Parent
-  #$ManifestPath = [System.IO.Path]::Combine($PWDParentDir,'AdminToolkit.psd1')
-  $ManifestPath = Join-Path -Path "AdminToolkit" -ChildPath "AdminToolkit.psd1"
   if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
-    Publish-Module -Path $ManifestPath -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
+    Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
   }
 } else {
   Write-Output "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
 }
-
