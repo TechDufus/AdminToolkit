d3b02f238e22e6560b7a8371645524ed0040f067
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-10 22:23:40 -0500

    Updating path to tests.

--- Build/azure-pipelines.ps1
+++ Build/azure-pipelines.ps1
@@ -2,5 +2,6 @@
       if (-Not($PesterModule.Version -gt '5.0.0')) {
         Install-Module -Name Pester -MinimumVersion 5.0.0 -Force -SkipPublisherCheck
       }
+      $TestsPath = [System.IO.Path]::Combine($PSScriptRoot,'..','Tests')
       
-      Invoke-Pester -Path "$PSScriptRoot/Tests/" -OutputFile "./Test-Pester.XML" -OutputFormat "NUnitXML"
+      Invoke-Pester -Path $TestsPath -OutputFile "./Test-Pester.XML" -OutputFormat "NUnitXML"
