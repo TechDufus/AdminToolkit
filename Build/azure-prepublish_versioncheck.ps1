445e445ea6b0a4c51e73ef60e83cf62be0ea83ae
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-11 18:47:53 -0500

    AzurePipeline config changes.
    Removing Parameter.
    Creating an environment variable for API Key.
    Using a test -WhatIf command before attempting publish.
    Removing Write-Error. Builds should succeed even if they do not publish to the gallery.

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -1,21 +1,15 @@
-param (
-  $PSGallery_Publish_API_Key
-)
-
 $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
 $CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine($PSScriptRoot,'..','AdminToolkit.psd1'))
 
-#How to handle INITIAL Publishing?
-#Must be able to detect both module versions
-#CurrentModule.Version must be -gt PSGalleryModule.Version
-
 if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
   #PIPELINE MODULE HAS A NEW MANIFEST VERSION - PUBLISH TO PSGALLERY OR INITIAL PUBLISHING TO PSGALLERY
-  #$NugetApiKey = $PSGallery_Publish_API_Key | ConvertFrom-SecureString -AsPlainText -Force
-  $PWDParentDir = Split-Path $PSScriptRoot -Parent
-  $ManifestPath = [System.IO.Path]::Combine($PWDParentDir,'AdminToolkit.psd1')
-  Publish-Module -Path $PWDParentDir -Repository PSGallery -NuGetApiKey $PSGallery_Publish_API_Key
+  #$PWDParentDir = Split-Path $PSScriptRoot -Parent
+  #$ManifestPath = [System.IO.Path]::Combine($PWDParentDir,'AdminToolkit.psd1')
+  $ManifestPath = Join-Path -Path "AdminToolkit" -ChildPath "AdminToolkit.psd1"
+  if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
+    Publish-Module -Path $ManifestPath -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
+  }
 } else {
-  Write-Error "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
+  Write-Output "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
 }
 
