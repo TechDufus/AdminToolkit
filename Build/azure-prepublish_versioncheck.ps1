2ae8b77d60739ce363f5127a24c6ef38934dc75e
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-11 21:44:17 -0500

    Please work..... 🤦‍♂️🤦‍♂️🤦‍♂️

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -1,7 +1,7 @@
 $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
-$CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine($env:Prep_Publish,"AdminToolkit","AdminToolkit.psd1"))
+$CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine("AdminToolkit","AdminToolkit.psd1"))
 if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
-  if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
+  if (Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
     Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
   }
 } else {
