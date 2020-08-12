1e483e751e2185cb175b86e64a0f567722f1b4da
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-11 19:43:49 -0500

    Error catching.

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -1,11 +1,15 @@
-$PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
-[System.String] $ManifestPath = $(Get-ChildItem "AdminToolkit.psd1" -Recurse).FullName
-$CurrentModule = Test-ModuleManifest -Path $ManifestPath
+Try {
+  $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
+  [System.String] $ManifestPath = $(Get-ChildItem "AdminToolkit.psd1" -Recurse).FullName
+  $CurrentModule = Test-ModuleManifest -Path $ManifestPath
 
-if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
-  if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
-    Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
+  if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
+    if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
+      Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
+    }
+  } else {
+    Write-Output "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
   }
-} else {
-  Write-Output "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
+} Catch {
+  Write-Error "$($_.Exception.Message)"
 }
