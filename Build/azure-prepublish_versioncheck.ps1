c0f812b36e8c8dc622fe29762d968381cf0a7cb0
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-11 22:00:59 -0500

    Update azure-prepublish_versioncheck.ps1

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -1,8 +1,8 @@
 $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
 $CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine("AdminToolkit","AdminToolkit.psd1"))
 if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
-  if (Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key -WhatIf) {
-    Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
+  if (Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGALLERY_API_KEY -WhatIf) {
+    Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGALLERY_API_KEY
   }
 } else {
   Write-Output "GitHub Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
