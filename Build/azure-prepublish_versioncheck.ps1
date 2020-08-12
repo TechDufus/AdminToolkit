c7060f665dad827581e86ea8d68c1553ee216ba9
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-11 20:30:22 -0500

    Please work. 😂

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -1,17 +1,12 @@
-# Try {
-#   $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
-#   Write-Output $PSScriptRoot
-#   $ManifestPath = Join-Path -Path $PSScriptRoot -ChildPath "AdminToolkit" -AdditionalChildPath "AdminToolkit.psd1"
-#   Write-Output $ManifestPath
-#   $CurrentModule = Test-ModuleManifest -Path $ManifestPath
+$PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
+#$ManifestPath = Get-ChildItem (Join-Path -Path ".." -ChildPath "AdminToolkit.psd1")
+#$ManifestPath = Join-Path -Path $PSScriptRoot -ChildPath "AdminToolkit" -AdditionalChildPath "AdminToolkit.psd1"
+$CurrentModule = Test-ModuleManifest -Path [System.IO.Path]::Combine($(System.DefaultWorkingDirectory),'AdminToolkit','AdminToolkit.psd1')
 
-#   if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
-#     if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
-#       Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
-#     }
-#   } else {
-#     Write-Output "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
-#   }
-# } Catch {
-#   Write-Error "$($_.Exception.Message)"
-# }
+if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
+  if (Publish-Module -Path  -Repository PSGallery -NuGetApiKey $PSGallery_API_Key -WhatIf) {
+    Publish-Module -Path $CurrentModule.Path -Repository PSGallery -NuGetApiKey $env:PSGallery_API_Key
+  }
+} else {
+  Write-Output "Current Module version ($($CurrentModule.Version)) | PSGallery Module version ($($PSGalleryModule.Version)). Pipeline module version is not greater than the Published PSGallery module version."
+}
