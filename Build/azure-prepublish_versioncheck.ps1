d56ef0ae8f71cc5dcc7ad7b67f48867047d2f78e
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-09-03 13:19:53 -0500

    Build module only after version check is valid.

--- Build/azure-prepublish_versioncheck.ps1
+++ Build/azure-prepublish_versioncheck.ps1
@@ -2,12 +2,18 @@
 $PSGalleryModule = Find-Module AdminToolkit -Repository PSGallery -ErrorAction SilentlyContinue
 
 #Get current GitHub Manifest version (From cloned repo in AzurePipelines)
-$CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine("AdminToolkit_Build","AdminToolkit","AdminToolkit.psd1"))
+$PipelineSourceModulePath = [System.IO.Path]::Combine('AdminToolkit','Build','azure-build.ps1')
+$PipelineSourceModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine('AdminToolkit','AdminToolkit.psd1'))
 
 #Ensure this is either an initial publish to PSGallery, or that the GitHub version is higher than the PSGallery version
-if ($CurrentModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
+if ($PipelineSourceModule.Version -gt $PSGalleryModule.Version -or (-Not($PSGalleryModule))) {
+  Write-Output 'Building Module:...'
+  & ([System.IO.Path]::Combine("$PSScriptRoot",'AdminToolkit','Build','azure-build.ps1'))
+  $CurrentModule = Test-ModuleManifest -Path ([System.IO.Path]::Combine("AdminToolkit_Build","AdminToolkit","AdminToolkit.psd1"))
+  
   Write-Output "PSGallery: v$($PSGalleryModule.Version)"
   Write-Output "Github: v$($CurrentModule.Version)"
+
   Write-Output "Attempting to publish new version $($CurrentModule.Version) to the PowerShell Gallery."
   Publish-Module -Path $CurrentModule.ModuleBase -Repository PSGallery -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose
 } else {
