c41f4384f00f132d9125eaa0707ecfc14bff2d2b
Matthew J. DeGarmo <matthewjdegarmo@gmail.com>
2021-03-18 11:06:02 -0500

    Adding parent process detection to relaunch correct pwsh applications.

--- Functions/Public/SU.ps1
+++ Functions/Public/SU.ps1
@@ -5,12 +5,21 @@
     Windows version of the linux command `SU`
 .DESCRIPTION
     Immitate SU on Linux. This creates new PoSH Session as an admin.
+.PARAMETER NoExit
+    Default `su` will close the current session and launch a new one as admin
+    Specify `-NoExit` to keep the non-admin shell running.
 .EXAMPLE
     PS> su
     
     Description
     -----------
-    Depending on what edition of powershell is running, this will start an elevated process.
+    Depending on what edition of powershell is running, this will start an elevated process and close the original process.
+.EXAMPLE
+    PS> su -NoExit
+
+    Description
+    -----------
+    This will keep the non-admin shell running and you will have two processes open.
 .NOTES
     Author: Matthew J. DeGarmo
     Handle: @matthewjdegarmo
@@ -21,10 +30,43 @@
 #>
 function su() {
     [CmdletBinding()]
-    param ()
+    param (
+        [Switch] $NoExit
+    )
     switch ($($PSVersionTable.PSEdition)) {
-        "Desktop" { Start-Process Powershell -Verb RunAs }
-        "Core" { Start-Process Pwsh -Verb RunAs }
+        "Desktop" { 
+            $Parent = (Get-Process -Id $PID)
+            Switch($Parent.ProcessName) {
+                'powershell_ise' {
+                    Start-Process $Parent.Path -Verb RunAs 
+                }
+                'Code' {
+                    #TODO: Detect VSCode running powershell.exe somehow
+                }
+
+                DEFAULT {
+                    Start-Process Powershell -Verb RunAs 
+                }
+            }
+
+        }
+        "Core" {
+            $Parent = (Get-Process -Id $PID).Parent
+            Switch($Parent.ProcessName) {
+                'WindowsTerminal' {
+                    Start-Process wt.exe -Verb RunAs
+                    If (-Not($NoExit.IsPresent)) {
+                        exit
+                    }
+                }
+                'Code' {
+                    Start-Process $Parent.Path -Verb RunAs
+                }
+                DEFAULT {
+                    Start-Process Pwsh -Verb RunAs
+                }
+            }
+        }
     }
 }
 #EndRegion SU
